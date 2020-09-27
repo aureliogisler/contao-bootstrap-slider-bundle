b4c02d49b28ca3d6971dc3fc38ec3ff02f2df240
xippoGmbH <aurelio.gisler@xippo.ch>
2020-09-27 15:19:27 +0200

    Initial

---
+++ README.md
@@ -0,0 +1,24 @@
+# Contao 4 Boostrap Slider Bundle
+
+This is a Slider for the Contao Content Managment Tool. It uses only
+bootstrap classes for definition. You can customizing the slider with 
+templates in the Contao Backend
+
+## Install
+
+Download the Bootstrap Slider Bundle:
+
+```bash
+wget https://github.com/xippogmbh/contao-bootstrap-slider-bundle/archive/master.zip
+unzip master.zip
+mv contao-bootstrap-slider-bundle-master [package name]
+cd [package name]
+```
+
+## Customize
+
+
+## Release
+
+
+[1]: https://contao.org
---
+++ src/ContaoManager/Plugin.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * This file is part of [package name].
+ *
+ * (c) John Doe
+ *
+ * @license LGPL-3.0-or-later
+ */
+
+namespace XippoGmbH\ContaoBootstrapSliderBundle\ContaoManager;
+
+use Contao\CoreBundle\ContaoCoreBundle;
+use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
+use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use XippoGmbH\ContaoBootstrapSliderBundle\ContaoBootstrapSliderBundle;
+
+class Plugin implements BundlePluginInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getBundles(ParserInterface $parser)
+    {
+        return [
+            BundleConfig::create(ContaoBootstrapSliderBundle::class)
+                ->setLoadAfter([ContaoCoreBundle::class]),
+        ];
+    }
+}
