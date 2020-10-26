d64e7f53c0cebde44a3210d8199ea2bbdab2bba2
xippoGmbH <aurelio.gisler@xippo.ch>
2020-10-26 20:13:30 +0100

    Update tl_content.php

--- src/Resources/contao/dca/tl_content.php
+++ src/Resources/contao/dca/tl_content.php
@@ -1,10 +1,6 @@
-// system/modules/news/dca/tl_content.php
-$GLOBALS['TL_DCA']['tl_content'] = [
-	'config' => [
-		'ptable' => 'tl_xippo_bs_slid'
-	],
-	'fields' => [
-		'boostrapSlider' => [
+// contao/dca/tl_content.php
+$GLOBALS['TL_DCA']['tl_content']['config']['ptable'] = 'tl_xippo_bs_slid';
+$GLOBALS['TL_DCA']['tl_content']['fields']['boostrapSlider'] = [
 			'label' => &$GLOBALS['TL_LANG']['tl_content']['bootStrapSlider'],
 			'inputType' => 'select',
 			'foreignKey' => 'tl_xippo_bs_slider.name',
@@ -12,9 +8,5 @@ $GLOBALS['TL_DCA']['tl_content'] = [
 				'mandatory' => true,
 				'includeBlankOption' => true
 			]
-		],
-	],
-	'palettes' => [
-		'bootstrapSlider' => '{type_legend},type,headline;{bootstrapSlider_legend},bootstrapSlider;{protected_legend:hide},protected;{expert_legend:hide},guests,invisible,cssID,space'
-	]
-];
+		];
+$GLOBALS['TL_DCA']['tl_content']['palettes']['bootstrapSlider'] = '{type_legend},type,headline;{bootstrapSlider_legend},bootstrapSlider;{protected_legend:hide},protected;{expert_legend:hide},guests,invisible,cssID,space';
