be42b5c37050d88619a0d1f65a0be0733b3c7c83
xippoGmbH <aurelio.gisler@xippo.ch>
2020-10-26 20:48:45 +0100

    Update tl_content.php

--- src/Resources/contao/dca/tl_content.php
+++ src/Resources/contao/dca/tl_content.php
@@ -1,7 +1,7 @@
 // contao/dca/tl_content.php
 $GLOBALS['TL_DCA']['tl_content']['config']['ptable'] = 'tl_xippo_bs_slid';
-$GLOBALS['TL_DCA']['tl_content']['fields']['boostrapSlider'] = [
-			'label' => &$GLOBALS['TL_LANG']['tl_content']['bootStrapSlider'],
+$GLOBALS['TL_DCA']['tl_content']['fields']['bootstrapSlider'] = [
+			'label' => &$GLOBALS['TL_LANG']['tl_content']['bootstrapSlider'],
 			'inputType' => 'select',
 			'foreignKey' => 'tl_xippo_bs_slider.name',
 			'sql' => ['type' => 'integer', 'unsigned' => true, 'notnull' => true, 'default' => 0],
